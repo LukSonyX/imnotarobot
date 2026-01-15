6975ac30fa7f12788ac419d0f5c9dfe7bd5221d5
LukSonyX <luke1072@email.cz>
2026-01-15 21:22:35 +0100

    adding jar file

---
+++ src/main/java/com/example/imnotarobot/Main.java
@@ -0,0 +1,9 @@
+package com.example.imnotarobot;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(ApplicationController.class, args);
+    }
+}
